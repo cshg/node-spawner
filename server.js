262d6e9e9cd636151f7e9214db6ebd5f6d139a83
cshg <csh@christianhaug.com>
2016-06-22 12:03:13 -0700

    Add form, routes and data file

--- server.js
+++ server.js
@@ -3,16 +3,12 @@ var Docker = require('dockerode');
 
 var port = process.env.PORT || 3000;
 
-var content = {
-  status: 'Ready to spawn docker instances'
-};
+var data = require('./data.json');
 
 var app = express();
 app.set('view engine', 'pug');
 
-app.get('/', function(req, res) {
-  res.render('index', content);
-});
+require('./routes')(app);
 
 app.listen(port, function() {
   console.log('Server listening on port ' + port);
